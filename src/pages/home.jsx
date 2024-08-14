0e2bfb6b4ebc2879328aa0826c21807569494854
ManiTyagi <manityagi1919@gmail.com>
2024-08-14 17:42:49 +0530

    Initial Commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const home = () => {
   return (
     <div>
       <h1 className='text-4xl'>Home</h1>
+      <Link to='/about'> About</Link>
     </div>
   )
 }
